1ae7be5efbb9307de4888121662b9653149c10e0
Gyang-eng <gyang52@emory.edu>
2025-04-26 20:59:56 -0400

    Update test.R

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -29,7 +29,7 @@ test_that("Multi View decomposition simulation runs", {
     V = 264,
     penalty = "SCAD",
     phi = 2,
-    psi = 1,
+    psi = 0.5,
     gamma = 2.1,
     rho = 0.95,
     espli1 = 1e-2,
